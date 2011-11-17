191c15faa20abfe8a3118b9fab0898d45316e1d5
Mitch Statz <mstatz@mail.com>
2011-11-17 15:59:11 -0700

    Added new files

---
+++ Wallboard/Scripts/jquery.wbelement.js
@@ -0,0 +1,35 @@
+﻿/*
+* Wall Board Element plugin 1.0
+*
+* Author: Mitchell Statz
+*/
+
+(function ($) {
+    $.fn.wbelement = function (options) {
+        var defaults = {
+            url: '',
+            delay: 60000
+        };
+        options = $.extend(defaults, options);
+        return this.each(function () {
+            var $this = $(this);
+            $this.url = options.url;
+            $this.delay = options.delay;
+            $this.updateWbElement($this);
+        });
+    };
+
+    $.fn.updateWbElement = function (el) {
+        $.fn.runUpdate(el);
+        setTimeout(function () { $.fn.updateWbElement(el); }, el.delay);
+    };
+
+    $.fn.runUpdate = function (el) {
+        $.ajax({
+            url: el.url,
+            success: function (data) {
+                el.html(data);
+            }
+        });
+    };
+})(jQuery);
